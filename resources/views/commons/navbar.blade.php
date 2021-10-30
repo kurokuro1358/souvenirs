fadffdea928bf8b2d6828449928d67e4ddbae2d2
kenta.kuroki <kurokuro1358@icloud.com>
2021-10-30 06:31:01 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,23 @@
+<header class="mb-4">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="/images/logo.png" class="img-fluid" width="150" alt="brand-image">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-bar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="nav-bar">
+      <ul class="navbar-nav ml-auto">
+        {{-- お土産を探すページへのリンク --}}
+        <li class="nav-item"><a href="#" class="nav-link">お土産を探す</a></li>
+        {{-- お土産を登録するページへのリンク --}}
+        <li class="nav-item"><a href="#" class="nav-link">お土産を登録する</a></li>
+        {{-- 会員登録ページへのリンク --}}
+        <li class="nav-item"><a href="#" class="nav-link">会員登録</a></li>
+        {{-- ログインページへのリンク --}}
+        <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+      </ul>
+    </div>
+  </nav>
+</header>
